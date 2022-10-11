133b3db5c6365f466fc5969716d064506c0e9c6d
neha3417 <neha3417@colorado.edu>
2022-10-11 11:03:40 -0600

    button

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,15 @@
 
-
-/* Selecting DOM element */
-const button = document.querySelector("button");
+const revealButton = document.querySelector(".reveal-button");
+const hiddenSection = document.querySelector(".hidden-section");
 
 /* Callback function */
-function alertButton() {
-    alert('Hi Person  ');
+function revealSection() {
+    if (hiddenSection.classList.contains("reveal")) {
+        hiddenSection.classList.remove("reveal");
+    } else {
+        hiddenSection.classList.add("reveal");
+    }
 }
 
 /* Event listener */
-button.addEventListener("click", alertButton, false);
+revealButton.addEventListener("click", revealSection, false);
