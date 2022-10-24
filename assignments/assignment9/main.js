394883d7616f4730f9db4ccac68febc783bc8dab
neha3417 <neha3417@colorado.edu>
2022-10-24 10:32:10 -0600

    carousel

--- assignments/assignment9/main.js
+++ assignments/assignment9/main.js
@@ -6,13 +6,44 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ['abstract.PNG', `david.PNG`, `forg.PNG`, `lilshop.PNG`, `museum.PNG`]
+
 /* Declaring the alternative text for each image file */
+
+const alt = {
+    'abstract.PNG' : 'abstract line portrait',
+    //'billie.PNG' : 'billie eilish digital portrait',
+    'david.PNG' : 'thermal portrait of david',
+    //'kermit.PNG' : 'astronaut kermit',
+    'lilshop.PNG' : 'little shop of horrors virtual playbill',
+    'museum.PNG' : 'ocean conservation museum'
+}
+
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alt[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', ev => {
+        displayedImage.scr = ev.target.src;
+        displayedImage.alt = ev.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      }
+});
