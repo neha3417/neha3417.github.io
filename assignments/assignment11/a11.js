c59b9b5348150ada56ecdd99be58f8b4e2195e15
neha3417 <neha3417@colorado.edu>
2022-11-03 17:08:22 -0600

    a11

---
+++ assignments/assignment11/a11.js
@@ -0,0 +1,7 @@
+
+const b = document.getElementById("js-new-quote");
+let image = document.getElementById("img");
+
+b.addEventListener("click", () =>{
+    image.src="https://gateway.marvel.com/v1/";
+});
