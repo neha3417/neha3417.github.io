c373d9343118607ec11724fa06d7cbac9e0ec6cb
neha3417 <neha3417@colorado.edu>
2022-11-06 21:16:51 -0700

    css

--- assignments/assignment11/a11.js
+++ assignments/assignment11/a11.js
@@ -1,7 +1,27 @@
 
-const b = document.getElementById("js-new-quote");
-let image = document.getElementById("img");
+// const b = document.getElementById("js-new-quote");
+// let image = document.getElementById("img");
 
-b.addEventListener("click", () =>{
-    image.src="https://gateway.marvel.com/v1/";
-});
+// b.addEventListener("click", () =>{
+//     image.src="https://gateway.marvel.com/v1/";
+// });
+
+// imgButton.addEventListener('click', () =>{
+//     randImg.src="https://dog.ceo/api/breeds/image/random";
+// });
+
+
+const randImg = document.getElementById('random-img');
+const imgButton = document.getElementById('btn-img');
+
+imgButton.addEventListener('click', fetchRandImg);
+
+function fetchRandImg() {
+  fetch('https://random.imagecdn.app/500/150')
+
+    .then(response => response.json())
+    .then(data => {
+        randImg.innerHTML = `<img src="${data.message}"/>`;
+
+  }) 
+}
