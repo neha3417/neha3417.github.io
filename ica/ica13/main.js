6d72163ce7d0c4ebe6be262f96a456429042d0a0
neha3417 <neha3417@colorado.edu>
2022-11-17 13:11:12 -0700

    ica13

--- ica/ica13/main.js
+++ ica/ica13/main.js
@@ -12,3 +12,33 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+// async function animatingAlice(){
+//     try {
+//         const animateAlice = await alice1.animate(aliceTumbling, aliceTiming).finished;
+//         return animateAlice;
+//     }
+//     catch(error){
+//         console.error(`Failed Alice Animation: ${error}`);  
+//     }
+// }
+
+async function animatingAlice(){
+    try {
+        const a1 = alice1.animate(aliceTumbling, aliceTiming);
+        await a1.finished;
+        const a2 = alice2.animate(aliceTumbling, aliceTiming);
+        await a2.finished;
+        const a3 = alice3.animate(aliceTumbling, aliceTiming);
+        await a3.finished;
+    }
+    catch(error){
+        console.error(`Failed Alice Animation: ${error}`);  
+    }
+}
+
+animatingAlice();
+
+// alice1.animate(aliceTumbling, aliceTiming).finished
+// .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+// .then(() => alice3.animate(aliceTumbling, aliceTiming).finished);
